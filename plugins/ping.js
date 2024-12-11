ec44808c6794cf45c272b58049f9dc196f852c92
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2024-12-11 21:35:36 +0530

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -31,14 +31,14 @@ eypz({
 async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
     try {
         const AmeenT = '📍ping...';
-        m.react(config.wait);
+        
         const start = new Date().getTime(); 
         const AmeenS = await conn.sendMessage(from, { text: AmeenT }, { quoted: mek });
         const end = new Date().getTime();
         const TripTime = end - start;
-        const AmeenET = '*🍁ꜱᴩᴇᴇᴅ!* ' + TripTime + ' *ms*';
+        const AmeenET = '_Response in ' + TripTime + ' ms_';
         await conn.sendMessage(from, { text: AmeenET, edit: AmeenS.key });
-        m.react(config.done);
+        
     } catch (e) {
         console.log(e);
         reply(`${e}`);
