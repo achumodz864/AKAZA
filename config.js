6a49990d91a62786208d2f43c5593f4f0473d297
Ajsal sparky <130032226+Axl-Sparky@users.noreply.github.com>
2024-12-13 10:29:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ ALIVE_IMG: process.env.ALIVE_IMG || "https://ik.imagekit.io/eypz/1727903040279_X
 ALIVE_MSG: process.env.ALIVE_MSG || "*I'm Still ALIVE🌸*",
 OWNER_NAME: process.env.OWNER_NAME || "Unknown-404",
 MENU: process.env.MENU || "https://ironman.koyeb.app/ironman/anime/waifu?type=long_hair",
-BOT_NAME: process.env.BOT_NAME || "OCTA-MD",
+BOT_NAME: process.env.BOT_NAME || "NEXTRO-MD",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 wait: "⏳",
 done: "✅",
